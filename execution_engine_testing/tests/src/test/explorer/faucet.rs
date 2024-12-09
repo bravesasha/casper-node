0192d6027fc25b4c71204bde19810ccdc3142889
Maciej Wójcik <maciek@wjck.pl>
2024-12-09 13:23:38 +0100

    update test values

--- execution_engine_testing/tests/src/test/explorer/faucet.rs
+++ execution_engine_testing/tests/src/test/explorer/faucet.rs
@@ -663,7 +663,7 @@ fn faucet_costs() {
     // This test will fail if execution costs vary.  The expected costs should not be updated
     // without understanding why the cost has changed.  If the costs do change, it should be
     // reflected in the "Costs by Entry Point" section of the faucet crate's README.md.
-    const EXPECTED_FAUCET_INSTALL_COST: u64 = 144_782_774_054;
+    const EXPECTED_FAUCET_INSTALL_COST: u64 = 156_027_934_448;
 
     const EXPECTED_FAUCET_SET_VARIABLES_COST: u64 = 70_836_755;
 
